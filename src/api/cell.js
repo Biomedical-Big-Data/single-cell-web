b083ca1d1cd5e7718745eb6c2e057629f43ff46c
gexiaowei <gandxiaowei@gmail.com>
2023-10-27 16:11:07 +0800

    🐞 fix: change cell mark to cell standard

--- src/api/cell.js
+++ src/api/cell.js
@@ -1,9 +1,9 @@
 import request from "@/utils/request.js"
 
-export function getCellTaxonomy(cell_marker) {
+export function getCellTaxonomy(cell_standard) {
   return request.get("/project/view/tree/cell_taxonomy", {
     params: {
-      cell_marker,
+      cell_standard,
     },
   })
 }
